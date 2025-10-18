3dede57b58b6cfc3a4bf53739ff5078f2dec0a5f
amicitia3600 <dyungwoo3600@gmail.com>
2025-10-19 04:18:22 +0900

    test 제발

--- Frontend/src/apis/apiClient.js
+++ Frontend/src/apis/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   timeout: 5000,
   withCredentials: true,
 });
